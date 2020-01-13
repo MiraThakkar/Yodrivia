5a640ddfd403380ad1fa92604d260acccb7b1c26
Shane Keney <shane@spikeball.com>
2020-01-13 16:08:32 -0500

    - Text inputs for index.js * Auto expanding to fit content for webpage

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,21 @@
-
-
 $(document).ready(function () {
 
     //call the function when load the page (prevent see the back of the card "anwsers")
     flipcard();
+
+    // Put all necessary Event Listeners below here //
+    //////////////////////////////////////////////////
 
     //bind functionality to sidebar
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
     });
+
+    document.addEventListener('input', function (event) {
+        if (event.target.tagName.toLowerCase() !== 'textarea') return;
+        autoExpand(event.target);
+    }, false);
 
     //event click on question card controlling the flip
     $(".card-question").click(function(){
@@ -21,3 +27,22 @@ $(document).ready(function () {
 function flipcard() {
     $(".card-question").flip({ axis: 'x' });
 }
+
+var autoExpand = function (field) {
+
+	// Reset field height
+	field.style.height = 'inherit';
+
+	// Get the computed styles for the element
+	var computed = window.getComputedStyle(field);
+
+	// Calculate the height
+	var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+	             + parseInt(computed.getPropertyValue('padding-top'), 10)
+	             + field.scrollHeight
+	             + parseInt(computed.getPropertyValue('padding-bottom'), 10)
+	             + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+
+	field.style.height = height + 'px';
+
+};
