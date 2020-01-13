d53db396f03b64c6daa666b859931d0634b9df1f
Shane Keney <shane@spikeball.com>
2020-01-13 13:11:16 -0500

    - minor changes to existing code for cleanup - moved flipCard function decl out of .ready

--- scripts/index.js
+++ scripts/index.js
@@ -5,29 +5,19 @@ $(document).ready(function () {
     //call the function when load the page (prevent see the back of the card "anwsers")
     flipcard();
 
-
     //bind functionality to sidebar
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
     });
 
-    /* ---------------------------------------------------
-    CARDS JQUERY
-    ----------------------------------------------------- */
-
-
-    //event click on question card
+    //event click on question card controlling the flip
     $(".card-question").click(function(){
         flipcard();
-    })
-
-    //function to flip the question card
-    function flipcard (){
-        $(".card-question").flip({
-            axis: 'x',
-        });
-    
-        }
-    
+    })   
 });
+
+//function to flip the question card
+function flipcard() {
+    $(".card-question").flip({ axis: 'x' });
+}
